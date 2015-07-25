648575f572bea32432c979e03163329d41c9fd64
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-07-26 01:54:08 +0200

    gulp: Add "watch" command

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,26 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 
+paths = {
+    files: ['LICENSE', 'README.md', 'index.html'],
+    scripts: 'main.js'
+};
+
 gulp.task('copy:files', function() {
-    return gulp.src(['LICENSE', 'README.md', 'index.html'])
+    return gulp.src(paths.files)
         .pipe(gulp.dest('out'));
 });
 
 gulp.task('build:js', function() {
-    return gulp.src('main.js')
+    return gulp.src(paths.scripts)
         .pipe(babel())
         .pipe(uglify())
         .pipe(gulp.dest('out'));
+});
+
+gulp.task('watch', ['default'], function() {
+    gulp.watch(paths.files, ['copy:files']);
+    gulp.watch(paths.scripts, ['build:js']);
 });
 
 gulp.task('default', ['build:js', 'copy:files']);

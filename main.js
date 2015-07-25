66a48abfed8863d34f66943c896800d4374e1e85
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-07-26 00:35:43 +0200

    Add marker

--- main.js
+++ main.js
@@ -30,14 +30,41 @@ geolocation.on('change', function() {
 // enable tracking
 geolocation.setTracking(true);
 
+var iconFeature = new ol.Feature({
+    geometry: new ol.geom.Point(ol.proj.transform([7, 51], 'EPSG:4326', 'EPSG:3857')),
+    name: 'Null Island',
+    population: 4000,
+    rainfall: 500
+});
+
+var iconStyle = new ol.style.Style({
+    image: new ol.style.Icon(({
+        anchor: [0.5, 46],
+        anchorXUnits: 'fraction',
+        anchorYUnits: 'pixels',
+        opacity: 0.75,
+        src: 'http://openlayers.org/en/v3.7.0/examples/data/icon.png'
+    }))
+});
+
+iconFeature.setStyle(iconStyle);
+
+var vectorSource = new ol.source.Vector({
+    features: [iconFeature]
+});
+
+var vectorLayer = new ol.layer.Vector({
+    source: vectorSource
+});
+
+var backgroundLayer = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+        url: 'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=' + MAPBOX_TOKEN
+    })
+});
+
 var map = new ol.Map({
     target: 'map',
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.XYZ({
-                url: 'https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=' + MAPBOX_TOKEN
-            })
-        })
-    ],
+    layers: [backgroundLayer, vectorLayer],
     view: view
 });

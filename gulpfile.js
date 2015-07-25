56372c86f9921a258e9d2d6984088fe2b3dc3235
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-07-26 01:18:00 +0200

    gulp: Use "babel" to process JS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
+var babel = require('gulp-babel');
 
-gulp.task('default', function() {
-    return gulp.src(['LICENSE', 'README.md', 'index.html', 'main.js'])
+gulp.task('copy:files', function() {
+    return gulp.src(['LICENSE', 'README.md', 'index.html'])
         .pipe(gulp.dest('out'));
 });
+
+gulp.task('build:js', function() {
+    return gulp.src('main.js')
+        .pipe(babel())
+        .pipe(gulp.dest('out'));
+});
+
+gulp.task('default', ['build:js', 'copy:files']);

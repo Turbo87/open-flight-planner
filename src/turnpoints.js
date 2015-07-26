3948e9aa28853fa711514cdbd62d66d335132595
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-07-26 03:25:37 +0200

    Turnpoints: Call changed() if turnpoint geometries change

--- src/turnpoints.js
+++ src/turnpoints.js
@@ -1,6 +1,13 @@
 export default class Turnpoints extends ol.Collection {
-    constructor() {
-        super([]);
+    constructor(...args) {
+        super(...args);
+        this.on('add', evt => {
+            evt.element.getGeometry().on('change', () => this.changed());
+            this.changed();
+        });
+        this.on('remove', evt => {
+            evt.element.getGeometry().un('change', () => this.changed());
+            this.changed();
+        });
     }
 }
-

58089bc6fba92ced3c754240f5f5ea44345ade12
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-07-28 15:39:08 +0200

    Gulp: Deactivate UglifyJS for now

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('build:js', function() {
         .pipe(source(paths.mainScript))
         .pipe(buffer())
         .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(uglify())
+        //.pipe(uglify())
         .on('error', gutil.log)
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('out'));
